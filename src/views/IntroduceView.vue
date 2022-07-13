87cfe44bc21fb1f2918f973caf78ff81658a97c5
HoonJae <rlagnswo0505@naver.com>
2022-07-13 12:27:47 +0900

    introduce computed

--- src/views/IntroduceView.vue
+++ src/views/IntroduceView.vue
@@ -2,6 +2,12 @@
   <h1>나의 이름은 {{ userName }}입니다.</h1>
   <input type="number" v-model.number="numberModel" />
   <div>{{ printNumber }}</div>
+  <div>
+    <label for="">여성<input type="radio" name="" v-model.number="gender" value="0" id="" /></label>
+    <label for="">남성<input type="radio" name="" v-model.number="gender" value="1" id="" /></label>
+  </div>
+  <div>{{ gender }}</div>
+  <div>{{ printGender }}</div>
 </template>
 
 <script>
@@ -10,11 +16,16 @@ export default {
     return {
       userName: '김재훈',
       numberModel: 3,
+      gender: 0,
     };
   },
+  // computed 값이 변하면 계산해서 주겠다
   computed: {
     printNumber() {
       return this.numberModel + 3;
+    },
+    printGender() {
+      return ~~this.gender === 0 ? '여성' : '남성';
     },
   },
 };
