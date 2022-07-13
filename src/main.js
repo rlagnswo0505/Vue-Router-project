fd29ff517d5bb1199a5aa8d5aeea2a4ee8ac5580
HoonJae <rlagnswo0505@naver.com>
2022-07-13 11:47:04 +0900

    introduce computed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+// 중간에 위치해서 연결해주는 것을 미들웨어라고 함
+createApp(App).use(router).mount('#app');
---
+++ src/views/IntroduceView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>나의 이름은 {{ userName }}입니다.</h1>
+  <input type="number" v-model.number="numberModel" />
+  <div>{{ printNumber }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '김재훈',
+      numberModel: 3,
+    };
+  },
+  computed: {
+    printNumber() {
+      return this.numberModel + 3;
+    },
+  },
+};
+</script>
+
+<style></style>
